b8bb6d87097146640b9f360f05249adca078ef3e
Cosmas-pro <91739410+Cosmas-pro@users.noreply.github.com>
2021-10-15 11:26:54 +0300

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,13 @@
+/*calculate the area of a rectangle*/
+#include<stdio.h>
+int main()
+{
+int length, width, Area;
+printf("\n Enter value to length:");
+scanf("%d",&length);
+printf("\n Enter value to width:");
+scanf("%d",&width);
+Area =length*width;
+printf("\n Area =&",Area);
+return 0;
+}
